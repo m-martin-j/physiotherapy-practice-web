38252f10cefaae748673f1becbcdfbbe8462e2ce
Martin Trat <trat@fzi.de>
2025-03-14 10:44:42 +0100

    Add style code
    animation fly-in,
    class font-family-heading

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,9 @@
+var windowLoaded = false;
+window.onload = function() {
+  windowLoaded = true;
+}
+
+// mobile menu
 var body = document.querySelector('body')
 var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
@@ -17,3 +23,31 @@ window.onscroll = function() {
     header.classList.remove('header-shrink');
   }
 }
+
+// keyframes flyIn animation
+const inViewport = (entry) => {
+  entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+};
+
+const handleIntersectionOnce = (entries, observer) => {
+  entries.forEach(entry => {
+    inViewport(entry);
+    if (entry.isIntersecting && windowLoaded) {
+      observer.unobserve(entry.target);  // handle intersection only once
+      // TODO: problem; if refreshed with the element in view,
+      //    the animation will trigger a second time
+      //    and only then, the observer will unobserve the element.
+    }
+  })
+};
+
+const obsOptions = {
+  rootMargin: '0% 0% -20% 0%',
+  threshold: 1.0
+  //https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+};
+const Obs = new IntersectionObserver(handleIntersectionOnce, obsOptions);
+
+document.querySelectorAll('[data-inviewport]').forEach(el => {
+  Obs.observe(el);
+});
